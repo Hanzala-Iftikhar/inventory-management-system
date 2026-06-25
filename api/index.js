6bdae580d98d96e2c8bc6edfdb2a6bcb6268d529
Hanzala <hanzalaiftikhar25@gmail.com>
2026-06-25 19:13:21 +0500

    fix: prisma generate via postinstall

--- api/index.js
+++ api/index.js
@@ -1,14 +1,2 @@
-const { execSync } = require('child_process');
-const path = require('path');
-
-// Generate Prisma client at runtime if not already generated
-try {
-  execSync('cd ' + path.join(__dirname, '../backend') + ' && node_modules/.bin/prisma generate', {
-    stdio: 'inherit'
-  });
-} catch (e) {
-  console.log('Prisma already generated');
-}
-
 const app = require('../backend/server');
 module.exports = app;
